0dd153524106a4ae1b18ff269bad4e558f01c836
stretchmans <70104808+stretchmans@users.noreply.github.com>
2020-08-24 01:59:46 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -18,16 +18,8 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
     
-async def on_message(message):
-    i = message.content
-    m = message.content.replace("/","")
-    text_list = m.split("d")
-    sum = 0
-    for i in range(int(text_list[0])):
-        one = random.randint(1, int(text_list[1]))
-        sum = sum + one
-    s = sum
-    
-    await client.send_massage(message.channel, s)
+async def ccb(message):
+    num = random.randint(1, 100)
+    await client.send_massage(message.channel, num)
 
 bot.run(token)
