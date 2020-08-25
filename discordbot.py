d9aa720c6898ca59ce29871fa455030baa076499
stretchmans <70104808+stretchmans@users.noreply.github.com>
2020-08-25 19:11:19 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,9 +3,6 @@ import os
 import traceback
 import random
 
-bot = commands.Bot(command_prefix='c')
-token = os.environ['DISCORD_BOT_TOKEN']
-
 client = discord.Client()
 client_id = conf['client_id']
 
@@ -17,10 +14,10 @@ async def on_ready():
 @client.event
 async def on_message(message):
     # 開始ワード
-    if message.content.startswith('dice'):
+    if message.content.startswith('!'):
         # 送り主がBotではないか
         if client.user != message.author:
-            info = parse('dice {}d{} {}', message.content)
+            info = parse('!{}d{} {}', message.content)
             if info:
                 if info[1].isdecimal() and info[0].isdecimal():
                     dice_num = int(info[0])
@@ -32,7 +29,7 @@ async def on_message(message):
                         m = temp_madness()
                     elif key == '不定の狂気':
                         m = ind_madness()
-                    elif key == 'dice':
+                    elif key == '!':
                         m = simple_dice(dice_size, dice_num)
                     else:
                         chara = get_charactor(str(message.author))
@@ -49,16 +46,70 @@ async def on_message(message):
 
 client.run(client_id)
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+def dice(dice_size):
+    num = np.random.randint(1, int(dice_size))
+    return num
 
+def simple_dice(dice_size, dice_num):
+    dice_val = np.array([], dtype=np.int64)
+    for i in range(dice_num):
+        dice_val = np.append(dice_val, dice(dice_size))
+    msg = str(np.sum(dice_val)) + str(dice_val)
+    return msg
 
-@bot.command()
-async def cb(ctx):
-    num = random.randint(1, 100)
-    await ctx.send(num)
+def judge(charactor, key, dice_size, dice_num):
+    dice_val = np.array([], dtype=np.int64)
+    for i in range(dice_num):
+        dice_val = np.append(dice_val, dice(dice_size))
+    if int(charactor[key]) >= np.sum(dice_val):
+        msg = key + ' ' + str(charactor[key]) + ' >= ' + str(np.sum(dice_val)) + ' = ' + str(dice_val)
+        if np.sum(dice_val) <= 5:
+            msg += ' 【クリティカル】'
+        msg += ' Success'
+        return msg, True
+    else:
+        msg = key + ' ' + str(charactor[key]) + ' < ' + str(np.sum(dice_val)) + ' = ' + str(dice_val)
+        if np.sum(dice_val) >= 96:
+            msg += ' 【ファンブル】'
+        msg += ' Fail'
+        return msg, False
+   
+def damage(charactor, key):
+    d = np.array([], dtype=np.int64)
+    if key == 'こぶし':
+        d = np.append(d, dice(3))
+    elif key == '頭突き':
+        d = np.append(d, dice(4))
+    elif key == 'キック':
+        d = np.append(d, dice(6))
+    else:
+        return None
+   
+def temp_madness():
+    roll = {}
+    roll[1] = '鸚鵡返し（誰かの動作・発言を真似することしか出来なくなる）'
+    #(中略)
+    roll[20] = '過信（自分を全能と信じて、どんなことでもしてしまう）'
+    msg = roll[dice(20)]
+    msg += '\n一時的狂気(' + str(dice(10)+4) + 'ラウンドまたは' + str(dice(6)*10+30) + '分)'
+    return msg
 
-bot.run(token)
+def ind_madness():
+    roll = {}
+    roll[1] = '失語症（言葉を使う技能が使えなくなる）'
+    #(中略)
+    roll[10] = '殺人癖（誰彼構わず殺そうとする） '
+    msg = roll[dice(10)]
+    msg += '\n不定の狂気(' + str(dice(10)*10) + '時間)'
+    return msg
+
+    if 'd' in charactor['db']:
+        result = parse('{}d{}', charactor['db'])
+        dice_size = int(result[1])
+        dice_num = int(result[0])
+        for i in range(np.abs(dice_num)):
+            if dice_num < 0:
+                d = np.append(d, -dice(dice_size))
+            else:
+                d = np.append(d, dice(dice_size))
+    return d
